e0ce1874287f7f2afb9daadb19622d671e334dc3
MCS-Kaijin <MCS-Kaijin@users.noreply.github.com>
2015-07-04 11:54:33 -0500

    Update main.py
    The program is now able to download GitHub repos.

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 # coding: utf-8
 
 import ui
-from sys import exit
-	
+import tempfile
+import zipfile
+import urllib
+from console import alert
+
+def getrepo(sender):
+	user=sender.superview.usr
+	repo=sender.superview.rep
+	with tempfile.NamedTemporaryFile(mode='w+b', suffix='.zip') as f:
+		urllib.urlretrieve('https://codeload.github.com/'+user+'/'+repo+'/zip/master',f.name)
+		z=zipfile.ZipFile(f)
+		z.extractall()
 
 class iGitHub(ui.View):
-	def side_bar_menu(self):
-		self["label1"].text="Hiya!"
-
 	def did_load(self):
-		self["side_bar"].action=self.side_bar_menu
+		
+		"""							"""
+		self.usr="MCS-Kaijin"		# GitHub user
+		self.rep="iGitHub"		# GitHub repo
+		"""There will be a way to change these in-app later...	"""
+		
+		self["sb"].action=getrepo
 
 gui = ui.load_view("iPhone_main")
-gui.present("fullscreen")
+gui.present("fullscreen", hide_title_bar=True)
