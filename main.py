bbf70eaf905db5e3cde0286ccd18155a66f34603
cclauss <cclauss@bluewin.ch>
2015-07-05 14:43:38 +0200

    Minor optimizations
    * urllib.urlretrieve() creates a tempfile that you can use directly.

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 # coding: utf-8
 
 import ui
-import tempfile
-import zipfile
 import urllib
-from console import alert
+import zipfile
 
 def getrepo(sender):
-	user=sender.superview.usr
-	repo=sender.superview.rep
-	with tempfile.NamedTemporaryFile(mode='w+b', suffix='.zip') as f:
-		urllib.urlretrieve('https://codeload.github.com/'+user+'/'+repo+'/zip/master',f.name)
-		z=zipfile.ZipFile(f)
-		z.extractall()
+    fmt = 'https://codeload.github.com/{usr}/{rep}/zip/master'
+    url = fmt.format(**sender.superview.__dict__)
+    filename, headers = urllib.urlretrieve(url)
+    with zipfile.ZipFile(filename) as zip_file:
+        zip_file.extractall()
 
 class iGitHub(ui.View):
-	def did_load(self):
-		
-		"""							"""
-		self.usr="MCS-Kaijin"		# GitHub user
-		self.rep="iGitHub"		# GitHub repo
-		"""There will be a way to change these in-app later...	"""
-		
-		self["sb"].action=getrepo
+    def did_load(self):
+        self.usr = "MCS-Kaijin"   # GitHub user
+        self.rep = "iGitHub"      # GitHub repo
+        """There will be a way to change these in-app later...  """
+        self["sb"].action=getrepo
 
-gui = ui.load_view("iPhone_main")
+gui = ui.load_view()  # ("iPhone_main")
 gui.present("fullscreen", hide_title_bar=True)
